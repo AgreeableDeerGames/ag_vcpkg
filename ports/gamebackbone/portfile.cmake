e95eab5d33382c06c5f9dfd95e5baf738394fe0c
Michael Gyurgyak <gyurgyma@mail.uc.edu>
2019-03-09 14:59:52 -0500

    Updated sha512 hash. Changed GameBackbone to lowercase

--- ports/gamebackbone/portfile.cmake
+++ ports/gamebackbone/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lavinrp/GameBackbone
     REF 4db5c754586e4269573e182980077f8f37adffc0
-    SHA512 1
+    SHA512 974f037fa95ad0bfbf2de94e163aebc3fbe90ed8c4f1119a0e53cf5dad3685834aa21997c69423360e6f4421c081fed7efffbcb2c73af78920e2969f9e01a983
     HEAD_REF feature/i164-GB_Consumable:
 )
 
