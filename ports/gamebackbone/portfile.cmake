3fd20c16376bd53a3546d5e7796e0d51e0e9566c
gyurgyma <thetemplarofanubis@gmail.com>
2019-04-27 16:27:51 -0400

    Update to point to GameBackbone 0.2.0 and master.

--- ports/gamebackbone/portfile.cmake
+++ ports/gamebackbone/portfile.cmake
@@ -3,9 +3,9 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lavinrp/GameBackbone
-    REF 4db5c754586e4269573e182980077f8f37adffc0
-    SHA512 974f037fa95ad0bfbf2de94e163aebc3fbe90ed8c4f1119a0e53cf5dad3685834aa21997c69423360e6f4421c081fed7efffbcb2c73af78920e2969f9e01a983
-    HEAD_REF feature/i164-GB_Consumable:
+    REF 0.2.0
+    SHA512 
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
