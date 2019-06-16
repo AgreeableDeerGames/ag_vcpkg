a126dab5b7153c4483954aa5faad487354e45f8d
Michael Gyurgyak <gyurgyma@mail.uc.edu>
2019-06-15 22:57:00 -0400

    Updated GameBackbone to 0.2.1

--- ports/gamebackbone/portfile.cmake
+++ ports/gamebackbone/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lavinrp/GameBackbone
-    REF 0.2.0
-    SHA512 6a0eb72fef8911ce4ae56163682150150a669d93cf65f24db2608112efb18fedefe314b415cf7d349cfb00a471788f43f85fde97191d6cb2f4be02f08c04b199
+    REF 0.2.1
+    SHA512 d59b5558d8055963060116fb73378338a165da595b42c1cd761e134ec4cad7cd63984f26576399365c2d433cf7b0536b6bb5817eaf8604ac217e92b1efbbadeb
     HEAD_REF master
 )
 
